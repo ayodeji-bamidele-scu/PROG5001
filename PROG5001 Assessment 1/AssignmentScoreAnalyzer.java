b2ad694c3c45291bd61df9e1c9ed58fcf1923db0
ayodeji-bamidele-scu <129622291+ayodeji-bamidele-scu@users.noreply.github.com>
2023-09-13 21:55:21 +1000

    Input F1 for Assignment Name
    Inputting the function to receive assignment name from users

--- PROG5001 Assessment 1/AssignmentScoreAnalyzer.java
+++ PROG5001 Assessment 1/AssignmentScoreAnalyzer.java
@@ -6,3 +6,11 @@
  * @version (1.0)
  */
 import java.util.Scanner; //Importing the Scanner class to read input from users
+public class AssignmentScoreAnalyzer{
+     public static void main(String[] args) {//Function to receive assignment name from users 
+         System.out.println("Input assignment name");
+         Scanner scanner = new Scanner(System.in);
+         String assignmentName = scanner.nextLine();
+         System.out.println("Assigment name: " + assignmentName);
+        }
+    }
