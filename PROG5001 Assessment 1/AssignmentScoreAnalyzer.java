53d0107cf5a5b2963a655bebc5a9406e75842c35
ayodeji-bamidele-scu <129622291+ayodeji-bamidele-scu@users.noreply.github.com>
2023-09-13 23:47:14 +1000

    Commit to address F2 to F4 of the assessment
    Function to receive assignment name and students' marks from users. Also, to ensure marks are within the range of 0 and 30

--- PROG5001 Assessment 1/AssignmentScoreAnalyzer.java
+++ PROG5001 Assessment 1/AssignmentScoreAnalyzer.java
@@ -7,10 +7,26 @@
  */
 import java.util.Scanner; //Importing the Scanner class to read input from users
 public class AssignmentScoreAnalyzer{
-     public static void main(String[] args) {//Function to receive assignment name from users 
-         System.out.println("Input assignment name");
+     public static void main(String[] args) {//Function to receive assignment name and students' marks from user 
+         System.out.println("Input assignment name"); //recieve assignment name
          Scanner scanner = new Scanner(System.in);
          String assignmentName = scanner.nextLine();
-         System.out.println("Assigment name: " + assignmentName);
+                 
+        //Input 30 students' marks for the assignment
+        double[] studentMarks = new double[30];
+        for (int i = 0; i < 30; i++) {
+            System.out.print("Enter students' marks for the assignment" + (i + 1) + ": ");
+             while (true) { //while loop to ensure valid mark is collected from users
+                 double mark = scanner.nextDouble();
+                 if (mark >= 0 && mark <=30) {
+                     studentMarks[i] = mark;
+                     break;
+                 }else {
+                     System.out.println("Invalid mark. Please enter a valid student mark between 0 and 30.");
+                    }
+                }
+            }
+            // Print assignment name to address F4
+            System.out.println("Assigment name: " + assignmentName);
         }
     }
